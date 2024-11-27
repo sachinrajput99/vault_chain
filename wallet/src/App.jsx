3ef90ff9ca62c5309f70218fd59cb5cbe2971ef3
sachinrajput99 <129744384+sachinrajput99@users.noreply.github.com>
2024-11-27 12:46:42 +0530

    first commit vault chain

--- wallet/src/App.jsx
+++ wallet/src/App.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line
-import React from "react"; 
+import React from "react";
 import "./App.css";
 import { useContext } from "react";
-import { Select } from "antd";
-import logo from "./assets/logo.png";
-import { chains } from "./constants";
-import { Routes, Route } from "react-router-dom"
+// import { Select } from "antd";
+// import logo from "./assets/logo.png";
+// import { chains } from "./constants";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RecoverAccount from "./components/RecoverAccount";
 import CreateAccount from "./components/CreateAccount";
@@ -14,28 +14,35 @@ import { WalletContext } from "./providers/WalletProvider";
 import LoginPage from "./components/LoginPage";
 
 function App() {
-  const { selectedChain, setSelectedChain, wallet, seedPhrase } = useContext(WalletContext);
+  const { wallet, seedPhrase } =
+    useContext(WalletContext);
 
   return (
-    <div className="App   bg-[#f0b28c] b bg-opacity-70  rounded ">
-      <header >
-        <img src={logo}   height={100}  alt="logo" className="headerLogo" />
-        <Select value={selectedChain} onChange={(value) => setSelectedChain(value)}
-          options={chains} className="dropdown" />
-      </header>
-      {
-        wallet && seedPhrase ?
-          <Routes>
-            <Route path="/your-wallet" element={<WalletView />} />
-          </Routes>
-          :
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recover" element={<RecoverAccount />} />
-            <Route path="/your-wallet" element={<CreateAccount />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-      }
+    <div className="App flex flex-col items-center justify-center    b bg-opacity-70  rounded bg-red-950 ">
+      
+      <div>
+        {/* <img src={logo}   height={100}  alt="logo" className="headerLogo" /> */}
+{/*        
+        <Select
+          value={selectedChain}
+          onChange={(value) => setSelectedChain(value)}
+          options={chains}
+          className="dropdown"
+        /> */}
+      </div>
+      
+      {wallet && seedPhrase ? (
+        <Routes>
+          <Route path="/your-wallet" element={<WalletView />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recover" element={<RecoverAccount />} />
+          <Route path="/your-wallet" element={<CreateAccount />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      )}
     </div>
   );
 }
--- wallet/src/components/Home.jsx
+++ wallet/src/components/Home.jsx
@@ -4,49 +4,60 @@ import { Button } from "antd";
 import logo from "../assets/logo.png";
 import { Navigate, useNavigate } from "react-router-dom";
 import { TOKEN_KEY } from "../constants";
+import { useContext } from "react";
+import { Select } from "antd";
+// import logo from "./assets/logo.png";
+import { chains } from "../constants";
+import { WalletContext } from "../providers/WalletProvider";
 
 function Home() {
   const navigate = useNavigate();
+
+  const { selectedChain, setSelectedChain } = useContext(WalletContext);
 
   return (
     <>
       {localStorage.getItem(TOKEN_KEY) ? (
         <Navigate to="/login" />
       ) : (
-        <div className="content  flex flex-col justify-center items-center ">
-          <h3 className="absolute top-14 font-semibold  text-black text-2xl">
-            Welcome to Flex Wallet
-          </h3>
-          {/* <h3 className=" absolute top-24 font-semibold  text-lg underline text-gray-500  ">
-            The decentralized web awaits
-          </h3> */}
+        <div className="content flex flex-col items-center  mt-16 bg-blue-900 ">
           <div
-            className=" flex flex-col items-center "
+            className=" flex flex-col items-center mb-16  "
             style={{ display: "flex ", gap: 10 }}
           >
-            <img  
+            <img
+              className="mb-6"
               src={logo}
               alt="logo"
               width={100}
               style={{ objectFit: "contain" }}
             />
-            <h5 className="text-black font-semibold " style={{ fontWeight: "semibold", fontSize: 20 }}>Flex Wallet</h5>
+            {/* <h5 className="text-black font-semibold " style={{ fontWeight: "semibold", fontSize: 20 }}>Flex Wallet</h5> */}
+            <h5 className="text-black font-semibold   text-2xl">
+              Welcome to Flex Wallet
+            </h5>
+
+            <Select
+              value={selectedChain}
+              onChange={(value) => setSelectedChain(value)}
+              options={chains}
+              className="dropdown"
+            />
           </div>
           <div className=" flex justify-end flex-row  gap-4">
-            <Button
-              type="default "
+            <button
               onClick={() => navigate("/your-wallet")}
-              className="frontPageButton   text-white  py-2 px-4 rounded "
+              className="frontPageButton   text-white  py-2 px-4 rounded whitespace-nowrap"
             >
               Create A Wallet
-            </Button>
-            <Button
-              className="frontPageButton text-white  py-2 px-4 rounded"
-              type="default"
+            </button>
+            <button
               onClick={() => navigate("/recover")}
+              className="frontPageButton   text-white  py-2 px-4 rounded whitespace-nowrap "
             >
+              {" "}
               Import your Wallet
-            </Button>
+            </button>
           </div>
         </div>
       )}
